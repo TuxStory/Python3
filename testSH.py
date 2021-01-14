83a83f8a5f9483da70db95fbece1eefba7f81576
TuxStory <ajm.even@gmail.com>
2021-01-14 23:22:16 +0100

    sh3

--- testSH.py
+++ testSH.py
@@ -1,9 +1,12 @@
 import sh
 from sh import ifconfig
 from sh import find
+from sh import uname
 # pip3 install sh
 
 print(sh.ls("-l","/home/antoine",color="auto"))
 print(ifconfig("wlp2s0"))
-p = sh.find("-name", "test.py", _bg=True)
+p = find("-name", "test.py", _bg=True)
 print(p)
+kernel = uname("-r")
+print("Version du noyau Linux : ",kernel)
