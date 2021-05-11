a0ade0b510d55238687d98c9dc19c4d529e8e79c
TuxStory <ajm.even@gmail.com>
2021-05-11 23:54:33 +0200

    combi2

--- combinaison.py
+++ combinaison.py
@@ -2,11 +2,18 @@ import itertools as it
 
 x=[1,2,4,5,8,9]
 y=[6,7,4,3]
+
+def fonction(Tab):
+    for i, combi in enumerate(Tab):
+        print (i,">>>", combi)
+    print ("Nombre : ",len(Tab))
+
+#Permutations
 #print(list(it.permutations(x, 4)))
 Combi = list(it.permutations(x, 4))
 Combi2 = list(it.permutations(y, 4))
 
-for i, combi in enumerate(Combi):
-    print (i,">>>", combi)
-
-print ("Nombre : ",len(Combi))
+#Affichage
+Result1=fonction(Combi)
+print("="*30)
+Result2=fonction(Combi2)
