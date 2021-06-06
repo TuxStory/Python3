4b3051145eb5150fdae7bd0732d8ad40802d0eba
TuxStory <ajm.even@gmail.com>
2021-06-07 00:39:42 +0200

    Euro

---
+++ Euro2021.py
@@ -0,0 +1,18 @@
+#Groupe
+GA=["Italie","Suisse","Turquie","Pays de Galles"]
+GB=["Belgique","Danemark","Finlande","Russie"]
+GC=["Autriche","Pays-Bas","Ukraine","Macédoine du Nord"]
+GD=["Angleterre","Croatie","Rep. Tchèque","Ecosse"]
+GE=["Plogne","Espagne","Suède","Slovaquie"]
+GF=["France","Allemagne","Portugal","Hongrie"]
+
+#Score
+ScoreA=[0,0,0,0]
+ScoreB=[0,0,0,0]
+ScoreC=[]
+ScoreD=[]
+ScoreE=[]
+ScoreF=[]
+
+print(GB[0])
+#need to think
