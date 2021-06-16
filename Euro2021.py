824c5b80053441810d76e400400a03dd7d2525a8
TuxStory <ajm.even@gmail.com>
2021-06-16 22:59:47 +0200

    ita

--- Euro2021.py
+++ Euro2021.py
@@ -17,12 +17,12 @@ def main():
     GF=["France","Allemagne","Portugal","Hongrie"]
 
     #Score
-    TabA={GA[0]:3,GA[1]:0,GA[2]:1,GA[3]:1}
-    TabB={GB[0]:3,GB[1]:0,GB[2]:3,GB[3]:0}
+    TabA={GA[0]:6,GA[1]:1,GA[2]:0,GA[3]:4}
+    TabB={GB[0]:3,GB[1]:0,GB[2]:3,GB[3]:3}
     TabC={GC[0]:3,GC[1]:3,GC[2]:0,GC[3]:0}
     TabD={GD[0]:3,GD[1]:0,GD[2]:3,GD[3]:0}
     TabE={GE[0]:0,GE[1]:1,GE[2]:1,GE[3]:3}
-    TabF={GF[0]:0,GF[1]:0,GF[2]:3,GF[3]:0}
+    TabF={GF[0]:3,GF[1]:0,GF[2]:3,GF[3]:0}
 
     #Phase Finale
     TabFinal=[]
