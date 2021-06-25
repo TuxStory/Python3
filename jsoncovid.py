6f8cab634c16f21bc24a5c077b31bcffb1aa324d
TuxStory <ajm.even@gmail.com>
2021-06-25 13:53:16 +0200

    Covid-19

--- jsoncovid.py
+++ jsoncovid.py
@@ -1,24 +1,43 @@
 import json
 import urllib.request
 
+#variable
 Total = 0
+UCI = 0
+
+#Files
+url = 'https://epistat.sciensano.be/Data/COVID19BE_MORT.json'
+url2 = 'https://epistat.sciensano.be/Data/COVID19BE_HOSP.json'
 
 #Downloading file
-print('Beginning file download with urllib2...')
-url = 'https://epistat.sciensano.be/Data/COVID19BE_MORT.json'
+print('Beginning files download with urllib2...')
 urllib.request.urlretrieve(url, 'COVID19BE_MORT.json')
+urllib.request.urlretrieve(url2, 'COVID19BE_HOSP.json')
 
 #Read file
 with open('COVID19BE_MORT.json') as f:
-  data = json.load(f)
+    data = json.load(f)
 
-# Iterating through the json list
+with open('COVID19BE_HOSP.json') as f2:
+    data2 = json.load(f2)
+
 for i in data:
-#    print(i['DATE'],i['REGION'],i['AGEGROUP'],i['SEX'],i['DEATHS']) #DATA fichier non complete -> plantage
-     print(i['DATE']," >>> ",i['DEATHS'])
+#     print(i['DATE']," >>> ",i['DEATHS'])
      Total = i['DEATHS'] + Total
-
-# Closing file
 f.close()
 
-print ("Total : ",Total)
+Date = data2[-1]['DATE'] #Valeur champ Date dans la dernière ligne
+
+for i in data2:
+    if i['DATE'] == Date:
+        UCI = i['TOTAL_IN_ICU'] + UCI
+
+print ("Sciensano Belgium")
+print ("Date : ",Date)
+print ("Total Décès : ",Total)
+print ("Total Soin Intensif :",UCI)
+
+#ancienne méthode pour la date 
+#for i in data2:
+#    Date = i['DATE']
+
