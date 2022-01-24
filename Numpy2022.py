acf269295484f66432741e8a9fcb9bdb91fdf9d8
TuxStory <ajm.even@gmail.com>
2022-01-25 00:01:36 +0100

    Numpy

--- Numpy2022.py
+++ Numpy2022.py
@@ -31,8 +31,10 @@ print("Indice élément max :",np.argmax(F))
 print("Valeur :",F[np.argmax(F)])
 print("Indice élément min :",np.argmin(F))
 print("Valeur :",F[np.argmin(F)])
-print(np.sum(F))
-print(np.prod(F))
+print("Somme de F: ",np.sum(F))
+print("Produit de F: ",np.prod(F))
+print("Valeur Max (F): ", F.max())
+print("Valeur Min (F): ", F.min())
 
 arr1 = np.array([1, 2, 3])
 arr2 = np.array([4, 5, 6])
@@ -46,3 +48,29 @@ print(x)
 E = np.sort(E)
 print(E)
 
+print("")
+D = np.random.randint(10, size=40).reshape(5,8)
+print(D)
+print("Dimension : ",D.ndim)
+print("Size : ",D.size)
+print("Shape : ",D.shape)
+
+print("")
+F = np.array([1, 2, 3, 4, 5, 6])
+G = np.array([3, 2, 6, 4, 1, 7])
+
+list_of_coordinates= list(zip(F, G))
+print(list_of_coordinates)
+for element in list_of_coordinates:
+    print (element)
+
+K = np.array([11, 11, 12, 13, 14, 15, 16, 17, 12, 13, 11, 14, 18, 19, 20])
+unique_values = np.unique(K)
+print(unique_values)
+unique_values, indices_list = np.unique(K, return_index=True)
+print(indices_list)
+L = np.flip(K)
+print(L)
+
+#You can save it as “filename.npy” with:
+#np.save('filename', a)
