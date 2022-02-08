de997da58cde92573ccc746b706b1f5bd45327f9
TuxStory <ajm.even@gmail.com>
2022-02-08 18:59:07 +0100

    maj

--- barcolor.py
+++ barcolor.py
@@ -1,10 +1,10 @@
 import numpy as np
 import matplotlib.pyplot as plt
- 
+
 objects = ('Python', 'C++', 'Java', 'Perl', 'Scala', 'Lisp')
 y_pos = np.arange(len(objects))
 performance = [10,8,6,4,2,1]
- 
+
 bar = plt.bar(y_pos, performance, align='center', alpha=0.5)
 plt.xticks(y_pos, objects)
 bar[1].set_color('red')
@@ -22,5 +22,5 @@ bar[4].set_label('Scala')
 bar[5].set_label('Lisp')
 plt.legend()
 plt.grid()
- 
+
 plt.show()
