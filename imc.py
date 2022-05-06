99d7f9776f15fc9211d643d07263579045fb87a5
TuxStory <ajm.even@gmail.com>
2022-05-06 23:58:41 +0200

    #

--- imc.py
+++ imc.py
@@ -20,6 +20,6 @@ YOUR_IMC = imc(float(Taille),float(Poid))
 print ("Votre imc est de :",round(YOUR_IMC,2))
 YOUR_BEST_POID = imci(float(Taille),22)
 print ("votre poid ideal devrait être de",round(YOUR_BEST_POID,2),"kg -> iMC = 22.")
-# round(var,decimal) permet d'arrondir un résultat float
+## round(var,decimal) permet d'arrondir un résultat float
 diff_poid = difpoid(float(Poid),float(YOUR_BEST_POID))
 print ("Différence de ",round(diff_poid,2),"kg.")
