24d64b73be4a71f4d93f8e1cb69a0225a02bf79d
TuxStory <ajm.even@gmail.com>
2022-11-19 23:30:26 +0100

    FC

--- Futurecoder.py
+++ Futurecoder.py
@@ -25,3 +25,64 @@ espace =''
 for caractere in nom:
     print(espace+caractere)
     espace+=" "
+print("------Ex4------")
+phrase = 'Hello World'
+phrase2 = ''
+First = True
+
+for caractere in phrase:
+    if First:
+        caractere = caractere.upper()
+        phrase2 += caractere
+        First = False
+    else:
+        caractere = caractere.lower()
+        phrase2 += caractere
+
+print(phrase2)
+print("------Ex5------")
+phrase = 'Hello World'
+phrase2 = ''
+Upper = True
+
+for caractere in phrase:
+    if Upper:
+        caractere = caractere.upper()
+        phrase2 += caractere
+        Upper = False
+    else:
+        caractere = caractere.lower()
+        phrase2 += caractere
+        Upper = True
+
+print(phrase2)
+
+print("------Ex6------")
+nom = 'Melissa'
+nouveau_nom = ''
+for c in nom:
+    if c == 's':
+        c = '$'
+    if c == "e":
+        c = '3'
+    if c == 'a':
+        c = '@'
+    nouveau_nom += c
+
+print(nouveau_nom)
+
+print("------Ex7------")
+adn = 'AGTAGCGTC'
+adn_inverse = ''
+for caractere in adn:
+    if caractere == 'A':
+        caractere = 'T'
+    elif caractere == 'T':
+            caractere = 'A'
+    elif caractere == 'G':
+        caractere = 'C'
+    elif caractere == 'C':
+            caractere = 'G'
+    adn_inverse += caractere
+
+print(adn_inverse)
