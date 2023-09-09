fd5be168f94413930df352bc4b1f200dc257cabd
TuxStory <ajm.even@gmail.com>
2023-09-09 22:58:17 +0200

    tqdm

--- tqdmtest.py
+++ tqdmtest.py
@@ -8,10 +8,10 @@ for char in tqdm(["a", "b", "c", "d"]):
 
 for i in trange(100):
     time.sleep(0.01)
-    
+
 for i in trange(100,ascii=True):
     time.sleep(0.01)
-     
+
 pbar = tqdm(["a", "b", "c", "d"])
 for char in pbar:
     time.sleep(0.25)
@@ -25,5 +25,5 @@ pbar.close()
 
 for i in trange(10,ascii=True):
     ping = subprocess.run(["ping","-c 1","192.168.1.1"],stdout=subprocess.PIPE,stderr=subprocess.PIPE)
-    #print(ping.stdout)
+    print(ping.stdout)
     #print(ping.stderr)
