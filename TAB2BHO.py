193a49a54f8c8b7ed3b9e62c11dac8e13b7e8ae9
TuxStory <ajm.even@gmail.com>
2023-09-09 23:26:26 +0200

    PTABLE

--- TAB2BHO.py
+++ TAB2BHO.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python3.8
+#!/usr/bin/python3
 
 from prettytable import PrettyTable
 
@@ -11,9 +11,12 @@ for i in range (16):
 	h = hex(i)
 	x.add_row([i,b[2:],o[2:],h[2:]])
 
-#print(x)
 x.align["Binaire"] = "r"
 print(x.get_string(title="Tableau de nombres"))
+
+#The issue is that you are using the prettytable package (c. 2013) and not the PTable package (c. 2015).
+#Both provide the prettytable module. Only the PTable package supports table titles.
+#pip3 install PTable
 
 #--------------------------------------------
 #mytable.add_column("Column1", [my_list_a[0], my_list_a[1], my_list_a[2])
