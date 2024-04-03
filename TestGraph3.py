710e8a3f8e2dc241146c2cdee9af18ce635b6390
TuxStory <ajm.even@gmail.com>
2024-04-04 01:03:29 +0200

    Corrections

--- TestGraph3.py
+++ TestGraph3.py
@@ -9,16 +9,16 @@ listex2=[] ; listey2=[]
 #	listey.append(i*i)
 #	listex2.append(i)
 #	listey2.append(i*i*i)
-	
+
 listex=[0,1, 3, 4, 6]
-listey=[0,2, 3, 5, 12]	
+listey=[0,2, 3, 5, 12]
 listex2=[0,1, 6, 9, 14]
 listey2=[0,4, 7, 8, 13]
 
-x=listex 
+x=listex
 y=listey
 x2=listex2
-y2=listey2	
+y2=listey2
 
 plt.plot(x, y, label="Test1")
 plt.plot(x2, y2, label="Test2")
--- picnicitems.py
+++ picnicitems.py
@@ -1,7 +1,7 @@
 import os
 
 def printPicnic(itemsDict, leftWidth, rightWidth):
-    print('PICNIC ITEMS'.center(leftWidth + rightWidth, '-'))
+    print(' PICNIC ITEMS '.center(leftWidth + rightWidth, '-'))
     for k, v in itemsDict.items():
         print(k.ljust(leftWidth, '.') + str(v).rjust(rightWidth))
 
