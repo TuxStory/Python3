97e4bae439ee227efd316ce61e2299b3b030d065
TuxStory <ajm.even@gmail.com>
2024-04-16 00:10:32 +0200

    tourne

--- tournepython2.py
+++ tournepython2.py
@@ -1,23 +1,16 @@
-#!/bin/user/pyhton
-
-import curses
 import time
+import cursor
 
-# Initializing curses
-stdscr = curses.initscr()
-
-# Setting the cursor to invisible by inserting 0 as argument
-curses.curs_set(0)
+cursor.hide()
+print ('\033[? 25l', end="")
 
 print ("\033[1;32m===================")
 print ("   Terminal V2.5   ")
 print ("===================\n")
+
 Theme='⣾⣽⣻⢿⡿⣟⣯⣷'
 
 for j in range(10):
-  for i in Theme:
+   for i in Theme:
     print ("\033[1;32m >", i,"Processing ... ",end="\r")
     time.sleep(0.1)
-
-# Restoring original state
-curses.endwin()
