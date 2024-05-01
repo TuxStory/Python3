7e0d08ed5450a2f541befda8ac6319e8c89ae6f1
TuxStory <ajm.even@gmail.com>
2024-05-01 11:43:43 +0200

    ubuntu

--- ubuntu.py
+++ ubuntu.py
@@ -6,10 +6,13 @@ from affichage import Affiche2
 Name = []
 Number = []
 
-with open('ubuntu.csv', newline='') as csvfile:
-    reader = csv.reader(csvfile)
-    for row in reader:
-        Name.append(row[0])
-        Number.append(row[2])
+def main():
+  with open('ubuntu.csv', newline='') as csvfile:
+      reader = csv.reader(csvfile)
+      for row in reader:
+          Name.append(row[0])
+          Number.append(row[2])
 
-Affiche2("Ubuntu",Number,Name,20,10)
+  Affiche2("Ubuntu",Number,Name,20,10)
+
+main()
