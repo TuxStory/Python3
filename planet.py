5f38e7b887a1e49cb978a694fb3a4c7d781d1c50
TuxStory <ajm.even@gmail.com>
2024-05-12 01:39:16 +0200

    planet

--- planet.py
+++ planet.py
@@ -3,12 +3,13 @@
 # version 0.3
 
 class Planet(object):
-  def __init__(self, name, typ, order, rayon, distancesol):
+  def __init__(self, name, typ, order, rayon, distancesol, jour):
     self.name = name
     self.typ = typ
     self.order = order
     self.rayon = rayon
     self.distancesol = distancesol
+    self.jour = jour
 
   def describe_planet(self):
     return "Nom : %s\nType :  %s\nPosition : %s\nRayon : %s\nDistance soleil : %s" % (self.name, self.typ, self.order, self.rayon ,self.distancesol)
@@ -19,22 +20,23 @@ class Planet(object):
     print ("Position".ljust(15), ": ",self.order)
     print ("Rayon".ljust(15), ": ",self.rayon," km")
     print ("Distance Soleil".ljust(15), ": ",self.distancesol, "Millions de km ")
+    print ("Duré du jour".ljust(15), ": ", self.jour)
 
 if __name__ == '__main__':
 
-  mercure = Planet("Mercure", "Planète tellurique", "1", "2439", "58")
-  venus = Planet("Venus", "Planète tellurique", "2", "6051", "108,2")
-  terre = Planet("Terre", "Planète tellurique", "3", "6371", "149,6")
-  mars = Planet("Mars", "Planète tellurique", "4", "3389", "225")
-  jupiter = Planet("Jupiter", "Géante gazeuse", "5", "69911", "778,5")
-  saturne = Planet("Saturne", "Géante gazeuse", "6", "58232", "1434")
-  uranus = Planet("Uranus", "Géante de glace", "7", "25362", "2871")
-  neptune = Planet("Neptune", "Géante de glace", "8", "24662", "4495")
+  mercure = Planet("Mercure", "Planète tellurique", "1", "2439", "58", "59j00")
+  venus = Planet("Venus", "Planète tellurique", "2", "6051", "108,2", "243j00")
+  terre = Planet("Terre", "Planète tellurique", "3", "6371", "149,6", "24h")
+  mars = Planet("Mars", "Planète tellurique", "4", "3389", "225", "24h37")
+  jupiter = Planet("Jupiter", "Géante gazeuse", "5", "69911", "778,5", "9h56")
+  saturne = Planet("Saturne", "Géante gazeuse", "6", "58232", "1434", "10h34")
+  uranus = Planet("Uranus", "Géante de glace", "7", "25362", "2871", "17h34")
+  neptune = Planet("Neptune", "Géante de glace", "8", "24662", "4495", "16h06")
 
   sysolaire = [mercure,venus,terre,mars,jupiter,saturne,uranus,neptune]
   for planet in sysolaire:
     #print (planet.describe_planet())
     planet.affichage()
-    print ("="*35)
+    print ("="*40)
 
   #terre.affichage()
